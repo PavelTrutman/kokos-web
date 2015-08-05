8937a0f9bf2e2b4fdda2ccbd1cd4669d05e5799e
Pavel Trutman <pavel.trutman@gmail.com>
2015-08-05 20:51:07 +0200

    Form defined in php.

--- www/diskuze.php
+++ www/diskuze.php
@@ -4,8 +4,47 @@
   include_once('../app/include.php');
   include_once('../app/carousel.php');
 
-  $data[0] = dibi::query('SELECT [Id], [Date], [Title], [Author], [Email], [Message] FROM [KFE_Board] WHERE [ParentId] = 0 ORDER BY [Date] DESC')->fetchAll();
+use Nette\Forms\Form;
 
+$form = new Form;
+$form->addSubmit('send', 'Odelat');
+
+$form->addText('name', 'Jméno:')
+  ->addRule(Form::MAX_LENGTH, 'S tou délkou jména to zas tak nepřeháněj. %d znaků ti nestačí?', 30);
+$form['name']
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->setRequired('Zadej prosím svoje jméno, ať víme, kdo jsi.');
+
+$form->addText('email', 'E-mail:')
+  ->addRule(Form::MAX_LENGTH, 'S tou délkou e-mailu to zas tak nepřeháněj. %d znaků ti nestačí?', 30);
+$form['email']
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->setRequired('Zadej prosím svoje jméno, ať víme, kdo jsi.')
+  ->addRule(Form::EMAIL, 'Asi máš chybu v e-mailu, takhle by vypadat neměl.');
+
+$form->addText('headline', 'Nadpis:')
+       ->addRule(Form::MAX_LENGTH, 'S tou délkou naspisu to zas tak nepřeháněj. %d znaků ti nestačí?', 30);
+$form['headline']
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->setRequired('Zadej svému příspěvku nějaký nadpis.');
+
+$form->addTextArea('text', 'Tvoje zpráva:')
+       ->addRule(Form::MAX_LENGTH, 'Tvůj příspěvěk je příliš dlouhý.', 10000);
+$form['text']
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->setRequired('Zapomněl jsi na text.');
+
+$form->addCheckbox('agree', 'Přečetl jsem si pravidla diskuze a respektuji je.')
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->addRule(Form::EQUAL, 'Je potřeba souhlasit s pravidly diskuze.', TRUE);
+
+$form->addCheckbox('captcha', 'Captcha.')
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->addRule(Form::EQUAL, 'Vyplň prosím captchu.', TRUE);
+
+$form->addSubmit('view', 'Náhled');
+
+$data[0] = dibi::query('SELECT [Id], [Date], [Title], [Author], [Email], [Message] FROM [KFE_Board] WHERE [ParentId] = 0 ORDER BY [Date] DESC')->fetchAll();
 
 printPosts($data, 0);
 
@@ -20,6 +59,7 @@ function printPosts(&$data, $id) {
   }
 }
 
+$template['form'] = $form;
 $template['data'] = $data;
 
 $latte->render('../templates/diskuze.latte', $template);
