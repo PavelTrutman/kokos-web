d379bd7a930ca2a708225afe4e3857ad31fd35b2
Pavel Trutman <pavel.trutman@gmail.com>
2015-08-06 22:03:18 +0200

    Added preview field to the form.

--- www/diskuze.php
+++ www/diskuze.php
@@ -7,6 +7,20 @@
 use Nette\Forms\Form;
 
 // form for adding posts
+
+$formVals = array(
+  'name' => 'Zatvrzelý KoKoSák',
+  'email' => 'zatvrdl@do.kokosu.cz',
+  'headline' => 'Víc příkladů, víc sérií!',
+  'text' => 
+'Milí KoKoSáci
+řešení KoKoSu mě strašně baví a nemohli by jste posílat série častěji s více příklady uvnitř? Já je totiž vyřeším všechny strašně rychle a pak nemám co dělat.
+
+Díky moc
+
+P.S. A dělejte taky častěji soustředění, jsou totiž super!',
+);
+
 $form = new Form;
 $form->addSubmit('send', 'Odelat')
   ->getControlPrototype()
@@ -14,27 +28,36 @@ $form->addSubmit('send', 'Odelat')
     ->setHtml('<span class="fa fa-paper-plane"></span>&nbsp;Odeslat')
     ->addClass('btn-primary');
 
+$form->addText('parent', '')
+  ->addConditionOn($form['send'], Form::SUBMITTED)
+  ->setRequired('Formulář nebyl korektně vyplněn. Zkus stránku znovu načíst a formulář odeslat znovu.');
+
 $form->addText('name', 'Jméno:')
+  ->setAttribute('placeholder', $formVals['name'])
   ->addRule(Form::MAX_LENGTH, 'S tou délkou jména to zas tak nepřeháněj. %d znaků ti nestačí?', 40);
 $form['name']
   ->addConditionOn($form['send'], Form::SUBMITTED)
   ->setRequired('Zadej prosím svoje jméno, ať víme, kdo jsi.');
 
 $form->addText('email', 'E-mail:')
+  ->setAttribute('placeholder', $formVals['email'])
   ->addRule(Form::MAX_LENGTH, 'S tou délkou e-mailu to zas tak nepřeháněj. %d znaků ti nestačí?', 40);
 $form['email']
+  ->setType('email')
   ->addConditionOn($form['send'], Form::SUBMITTED)
   ->setRequired('Zadej prosím svoje jméno, ať víme, kdo jsi.')
   ->addRule(Form::EMAIL, 'Asi máš chybu v e-mailu, takhle by vypadat neměl.');
 
 $form->addText('headline', 'Nadpis:')
-       ->addRule(Form::MAX_LENGTH, 'S tou délkou naspisu to zas tak nepřeháněj. %d znaků ti nestačí?', 40);
+  ->setAttribute('placeholder', $formVals['headline'])
+  ->addRule(Form::MAX_LENGTH, 'S tou délkou naspisu to zas tak nepřeháněj. %d znaků ti nestačí?', 40);
 $form['headline']
   ->addConditionOn($form['send'], Form::SUBMITTED)
   ->setRequired('Zadej svému příspěvku nějaký nadpis.');
 
 $form->addTextArea('text', 'Tvoje zpráva:')
-       ->addRule(Form::MAX_LENGTH, 'Tvůj příspěvěk je příliš dlouhý.', 10000);
+  ->setAttribute('placeholder', $formVals['text'])
+  ->addRule(Form::MAX_LENGTH, 'Tvůj příspěvěk je příliš dlouhý.', 10000);
 $form['text']
   ->addConditionOn($form['send'], Form::SUBMITTED)
   ->setRequired('Zapomněl jsi na text.');
@@ -89,6 +112,7 @@ function printPosts(&$data, $id) {
 }
 
 $template['form'] = $form;
+$template['formVals'] = $formVals;
 $template['data'] = $data;
 
 $latte->render('../templates/diskuze.latte', $template);
